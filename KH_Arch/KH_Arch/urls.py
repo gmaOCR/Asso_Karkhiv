025894838f50506ec2d7388337740461dce052a9
greg <marianigregory@pm.me>
2023-12-06 17:16:21 +0100

    adding get-content-objects/

--- KH_Arch/KH_Arch/urls.py
+++ KH_Arch/KH_Arch/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from gallery import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('get-content-objects/', views.get_content_objects, name='get-content-objects'),
 ]
