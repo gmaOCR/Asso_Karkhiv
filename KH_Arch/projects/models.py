3b4a606753fd30b483725d8c1cb3d30e743fe6fb
greg <marianigregory@pm.me>
2023-12-06 16:57:45 +0100

    adding title

--- KH_Arch/projects/models.py
+++ KH_Arch/projects/models.py
@@ -2,10 +2,10 @@ from django.db import models
 
 
 class Project(models.Model):
+    title = models.CharField(max_length=50, default="Example")
     description = models.TextField()
     place = models.CharField(max_length=100)
     date = models.DateField()
-    photos = models.ManyToManyField('gallery.Photo', related_name='projects')
 
     def __str__(self):
         return self.description
