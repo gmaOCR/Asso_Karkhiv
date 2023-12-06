5a401df6e5ca40d28dbd3bac5c6b384bfcb55c35
greg <marianigregory@pm.me>
2023-12-06 17:16:35 +0100

    adding get-content-objects

--- KH_Arch/gallery/views.py
+++ KH_Arch/gallery/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.http import JsonResponse
+from django.contrib.contenttypes.models import ContentType
+
+
+def get_content_objects(request):
+    """
+   Renvoi
+    """
+    content_type_id = request.GET.get('content_type_id')
+    if content_type_id:
+        content_type = ContentType.objects.get_for_id(content_type_id)
+        objects = content_type.model_class().objects.all()
+        return JsonResponse({
+            'objects': [{'id': obj.id, 'name': str(obj)} for obj in objects]
+        })
+    return JsonResponse({'objects': []})
