007db1d3e078e2a27421dc70b966cec478ae86cc
greg <marianigregory@pm.me>
2023-12-08 15:13:17 +0100

    adding config custom for app Projects

--- KH_Arch/KH_Arch/settings.py
+++ KH_Arch/KH_Arch/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
-    'projects',
+    'projects.apps.ProjectsConfig',
     'events',
     'gallery',
     'home',
