a1e22a400b2b2b91c2eac9a195ca3b21016a9dbd
greg <marianigregory@pm.me>
2023-12-08 15:14:00 +0100

    adding File model and thumbnail fun

--- KH_Arch/gallery/models.py
+++ KH_Arch/gallery/models.py
@@ -2,9 +2,10 @@ import os
 
 from django.contrib.contenttypes.fields import GenericForeignKey
 from django.contrib.contenttypes.models import ContentType
+from django.core.exceptions import ValidationError
 from django.core.files.base import ContentFile
 from django.db import models
-from PIL import Image
+from PIL import Image, UnidentifiedImageError
 from io import BytesIO
 from django.core.files.uploadedfile import InMemoryUploadedFile
 import sys
@@ -52,32 +53,39 @@ class Photo(models.Model):
         super(Photo, self).delete(*args, **kwargs)
 
     def create_thumbnail(self):
-        # Ouvrez l'image originale
-        if hasattr(self.image.file, 'path'):
-            img = Image.open(self.image.path)
-        else:
-            # Si le fichier est en mémoire (par exemple, lors de l'upload), utilisez directement
-            img = Image.open(BytesIO(self.image.read()))
+        try:
+            # Ouvrez l'image originale
+            print(hasattr(self.image.file, 'path'))
+            if hasattr(self.image.file, 'path'):
+                img = Image.open(self.image.path)
+            else:
+                self.image.seek(0)
+                img_file = BytesIO(self.image.read())
+                img_file.seek(0)  # Rembobinez au début du fichier
+                img = Image.open(img_file)
 
-        # Définissez la taille de la miniature
-        output_size = (128, 128)
-        img.thumbnail(output_size)
+            # Définissez la taille de la miniature
+            output_size = (128, 128)
+            img.thumbnail(output_size)
 
-        # Sauvegardez la miniature dans un objet BytesIO
-        thumb_io = BytesIO()
-        img.save(thumb_io, format='JPEG', quality=85)
-        thumb_io.seek(0)
+            # Sauvegardez la miniature dans un objet BytesIO
+            thumb_io = BytesIO()
+            img.save(thumb_io, format='JPEG', quality=85)
+            thumb_io.seek(0)
 
-        # Construisez le nom du fichier de la vignette
-        thumbnail_name = self._get_thumbnail_name()
+            # Construisez le nom du fichier de la vignette
+            thumbnail_name = self._get_thumbnail_name()
 
-        # Créez un nouvel objet InMemoryUploadedFile pour la miniature
-        thumbnail_file = InMemoryUploadedFile(
-            thumb_io, 'ImageField', thumbnail_name, 'image/jpeg', sys.getsizeof(thumb_io), None
-        )
+            # Créez un nouvel objet InMemoryUploadedFile pour la miniature
+            thumbnail_file = InMemoryUploadedFile(
+                thumb_io, 'ImageField', thumbnail_name, 'image/jpeg', sys.getsizeof(thumb_io), None
+            )
 
-        # Sauvegardez le fichier de la vignette dans le champ thumbnail
-        self.thumbnail.save(thumbnail_name, thumbnail_file, save=False)
+            # Sauvegardez le fichier de la vignette dans le champ thumbnail
+            self.thumbnail.save(thumbnail_name, thumbnail_file, save=False)
+
+        except UnidentifiedImageError as e:
+            print(f"Erreur lors de la création de la miniature : {e}")
 
     def _get_thumbnail_name(self):
         # Retirez l'extension du fichier original et ajoutez '_thb'
@@ -87,3 +95,24 @@ class Photo(models.Model):
 
     def __str__(self):
         return self.description
+
+
+class File(models.Model):
+    file = models.FileField(upload_to='media/files')
+    project = models.ForeignKey('projects.Project', on_delete=models.CASCADE, null=True, blank=True)
+    event = models.ForeignKey('events.Event', on_delete=models.CASCADE, null=True, blank=True)
+
+    def clean(self):
+        # Vérifiez si à la fois projet et event sont définis ou si aucun des deux n'est défini
+        if (self.project and self.event) or (not self.project and not self.event):
+            raise ValidationError("Cannot be linked to both (event and project)")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
+    def delete(self, *args, **kwargs):
+        if self.file and os.path.isfile(self.file.path):
+            os.remove(self.file.path)
+            print(f"Fichier {self.file.path} supprimé.")
+        super().delete(*args, **kwargs)
