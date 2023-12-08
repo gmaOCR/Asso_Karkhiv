0c12ec0f55b9fe7a8845aa295f4947fd6e40010f
greg <marianigregory@pm.me>
2023-12-08 15:12:29 +0100

    change get_content

--- KH_Arch/gallery/views.py
+++ KH_Arch/gallery/views.py
@@ -5,14 +5,12 @@ from django.contrib.contenttypes.models import ContentType
 
 
 def get_content_objects(request):
-    """
-   Renvoi
-    """
     content_type_id = request.GET.get('content_type_id')
-    if content_type_id:
-        content_type = ContentType.objects.get_for_id(content_type_id)
-        objects = content_type.model_class().objects.all()
-        return JsonResponse({
-            'objects': [{'id': obj.id, 'name': str(obj)} for obj in objects]
-        })
+    content_type = ContentType.objects.get_for_id(int(content_type_id))
+    model = content_type.model_class()
+
+    if model:
+        objects = model.objects.all()
+        data = [{'id': obj.id, 'name': f"{obj.title} (ID: {obj.id})"} for obj in objects]
+        return JsonResponse({'objects': data})
     return JsonResponse({'objects': []})
