c904e3ae5e3d2263575f763c656764a18957f5fa
greg <marianigregory@pm.me>
2023-12-10 16:56:18 +0100

    adding categories

--- KH_Arch/projects/models.py
+++ KH_Arch/projects/models.py
@@ -1,11 +1,25 @@
 from django.db import models
 
 
+class ProjectType(models.TextChoices):
+    ARCHITECTURE = 'ar', 'Architecture'
+    URBANISM = 'ur', 'Urbanism'
+    RESEARCH = 're', 'Research'
+    PUBLICATION = 'pu', 'Publication'
+    INTERVIEW = 'in', 'Interview'
+    WORKSHOP = 'wo', 'Workshop'
+
+
 class Project(models.Model):
     title = models.CharField(max_length=50, default="Example")
     description = models.TextField()
     place = models.CharField(max_length=100)
     date = models.DateField()
+    type = models.CharField(
+        max_length=2,
+        choices=ProjectType.choices,
+        default=ProjectType.ARCHITECTURE
+    )
 
     def __str__(self):
         return self.description
