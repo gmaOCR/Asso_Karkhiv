2200b6d21799684eb66f97f50cdd47127afda81d
greg <marianigregory@pm.me>
2023-12-15 20:55:52 +0100

    adding project_detail

--- KH_Arch/projects/views.py
+++ KH_Arch/projects/views.py
@@ -1,23 +1,27 @@
 from django.http import Http404
-from django.shortcuts import render
-from .models import ProjectType, Project
+from django.shortcuts import render, get_object_or_404
+from .models import  Project
+from gallery.models import PhotoProject, File
 
 
 def project_list_by_type(request, type_code):
-    # Vérifie si le type_code est dans ProjectType.choices
-    type_name = next((name for code, name in ProjectType.choices if code == type_code), None)
-    if type_name is None:
-        # Si le type n'est pas trouvé, renvoyez une erreur 404
-        raise Http404("Project type not found.")
-
-    # Filtre les projets par le type sélectionné
-    projects = Project.objects.filter(type=type_code).prefetch_related('photos', 'files')
-
-    # Incluez également la liste complète des types pour la navbar
-    types = ProjectType.choices
+    projects, type_name = Project.get_by_type(type_code)
+    if projects is None:
+        raise Http404(type_name)
 
     return render(request, 'projects/projects_type.html', {
         'type_name': type_name,
-        'types': types,
         'projects': projects
+    })
+
+
+def project_detail(request, type_code, project_id):
+    project = get_object_or_404(Project, pk=project_id)
+    photos = PhotoProject.get_photos_for_project(project)
+    files = File.get_files_for_project(project)
+
+    return render(request, 'projects/project_detail.html', {
+        'project': project,
+        'photos': photos,
+        'files': files
     })
