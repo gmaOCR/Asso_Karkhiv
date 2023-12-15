db2715b3365facf9a8cce3a5d8fb192f00fd654a
greg <marianigregory@pm.me>
2023-12-15 20:50:25 +0100

    adding rouge and DEBUG

--- KH_Arch/KH_Arch/urls.py
+++ KH_Arch/KH_Arch/urls.py
@@ -14,15 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from gallery import views as gallery_views
 from home import views as home_views
 from projects import views as project_views
 
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('get-content-objects/', gallery_views.get_content_objects, name='get-content-objects'),
     path('', home_views.home_page, name='home'),
     path('type/<str:type_code>/', project_views.project_list_by_type, name='projects_type'),
+    path('type/<str:type_code>/<int:project_id>/', project_views.project_detail, name='project_detail'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
