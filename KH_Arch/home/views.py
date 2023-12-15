a7711f3ce527bebfed656e2f58858f428fe46481
greg <marianigregory@pm.me>
2023-12-15 20:55:40 +0100

    context variable added

--- KH_Arch/home/views.py
+++ KH_Arch/home/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
-
 from projects.models import ProjectType
 
 
 def home_page(request):
     types = ProjectType.choices
-    return render(request, 'home/base.html', {'types': types})
+    context = {
+        'types': types,
+    }
+    return render(request, 'home/base.html', context)
 
     
