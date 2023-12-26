7c4490c885e9a29b41a9083c7d4ab4f2d787cef4
greg <marianigregory@pm.me>
2023-12-26 20:49:53 +0100

    adding datas for carousel

--- KH_Arch/home/views.py
+++ KH_Arch/home/views.py
@@ -1,12 +1,18 @@
+from django.utils import timezone
+
 from django.shortcuts import render
-from projects.models import ProjectType
+from projects.models import ProjectType, Project
+from events.models import Event
 
 
 def home_page(request):
     types = ProjectType.choices
+    latest_project = Project.objects.order_by('-date').first()
+    upcoming_event = Event.objects.filter(date__gte=timezone.now()).order_by('date').first()
+
     context = {
         'types': types,
+        'latest_project': latest_project,
+        'upcoming_event': upcoming_event,
     }
-    return render(request, 'home/base.html', context)
-
-    
+    return render(request, 'home/home.html', context)
