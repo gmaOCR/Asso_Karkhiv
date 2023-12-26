58984736b37df67c347382679787c3e2060e8970
greg <marianigregory@pm.me>
2023-12-26 20:47:41 +0100

    refactoring logic

--- KH_Arch/gallery/signals.py
+++ KH_Arch/gallery/signals.py
@@ -1,31 +1,21 @@
-import os
-
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
 from .models import PhotoEvent, PhotoProject, File
 
 
-@receiver(pre_delete, sender=PhotoProject)
-def delete_photo_files(instance, **kwargs):
-    # Supprimez le fichier image associé s'il existe
-    if instance.image and os.path.isfile(instance.image.path):
-        os.remove(instance.image.path)
-    # Supprimez le fichier miniature associé s'il existe
-    if instance.thumbnail and os.path.isfile(instance.thumbnail.path):
-        os.remove(instance.thumbnail.path)
+def delete_files_on_pre_delete(sender, instance, **kwargs):
+    # Pour les modèles PhotoProject et PhotoEvent
+    if isinstance(instance, (PhotoProject, PhotoEvent)):
+        if instance.image:
+            instance.image.delete(save=False)
+        if instance.thumbnail:
+            instance.thumbnail.delete(save=False)
+
+    # Pour le modèle File
+    elif isinstance(instance, File):
+        if instance.file:
+            instance.file.delete(save=False)
 
 
-@receiver(pre_delete, sender=PhotoEvent)
-def delete_photo_files(instance, **kwargs):
-    # Supprimez le fichier image associé s'il existe
-    if instance.image and os.path.isfile(instance.image.path):
-        os.remove(instance.image.path)
-    # Supprimez le fichier miniature associé s'il existe
-    if instance.thumbnail and os.path.isfile(instance.thumbnail.path):
-        os.remove(instance.thumbnail.path)
-
-
-@receiver(pre_delete, sender=File)
-def delete_file_on_delete(instance, **kwargs):
-    if instance.file and os.path.isfile(instance.file.path):
-        os.remove(instance.file.path)
+for model in [PhotoProject, PhotoEvent, File]:
+    pre_delete.connect(delete_files_on_pre_delete, sender=model)
